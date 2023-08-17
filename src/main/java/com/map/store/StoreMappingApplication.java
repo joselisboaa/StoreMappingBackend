f2b8f1aa7198ebe2a081cb018359a90d33954298
1mmanuelKant <joselisboaa05@gmail.com>
2023-08-16 21:45:01 -0300

    initial: main application

---
+++ src/main/java/com/map/store/StoreMappingApplication.java
@@ -0,0 +1,13 @@
+package com.map.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMappingApplication.class, args);
+	}
+
+}
