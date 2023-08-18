a6b9dca4808e0666b0be314060c500b099849228
1mmanuelKant <joselisboaa05@gmail.com>
2023-08-17 22:09:18 -0300

    feat: client model entity

---
+++ src/main/java/com/map/store/domain/model/Client.java
@@ -0,0 +1,22 @@
+package com.map.store.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+}
