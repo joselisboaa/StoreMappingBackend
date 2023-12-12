f3fc689c330a599d182d75aeb1b0f4c2e92680db
joselisboaa <joselisboaa05@gmail.com>
2023-12-12 14:13:45 -0300

    feat: create city service implementation

---
+++ src/main/java/com/map/store/domain/service/CityServiceImpl.java
@@ -0,0 +1,67 @@
+package com.map.store.domain.service;
+
+import com.map.store.domain.model.City;
+import com.map.store.domain.repository.ICityRepository;
+import com.map.store.domain.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CityServiceImpl implements ICityService {
+    @Autowired
+    private ICityRepository cityRepository;
+
+    private final String CITY_NOT_EXIST = "Essa cidade não existe ou não foi encontrada.";
+
+     public List<City> getAll() {
+         return cityRepository.findAll();
+     }
+
+     public City getById(Long id) {
+         Optional<City> client = cityRepository.findById(id);
+
+        return client.orElseThrow(() -> {
+            throw new ResourceNotFoundException(CITY_NOT_EXIST);
+        });
+     }
+
+     public City create(City client) {
+         City newCity = cityRepository.save(client);
+
+         return newCity;
+     }
+
+     public City update(City newCityData, Long id) {
+         try {
+             City clientData = cityRepository.getReferenceById(id);
+             updateCityData(clientData, newCityData);
+             
+             if (clientData == null) {
+                 throw new ResourceNotFoundException("Teste");
+             }
+
+             return clientData;
+         } catch (EntityNotFoundException error) {
+             throw new ResourceNotFoundException(CITY_NOT_EXIST);
+         }
+     }
+
+     public void deleteById(Long id) {
+         try {
+             if (cityRepository.existsById(id)) {
+                 cityRepository.deleteById(id);
+             }
+             throw new EntityNotFoundException();
+         } catch (EntityNotFoundException error) {
+             throw new ResourceNotFoundException(CITY_NOT_EXIST);
+         }
+     }
+
+     private void updateCityData(City oldCityData, City newCityData) {
+         oldCityData.setName(newCityData.getName());
+     }
+}
