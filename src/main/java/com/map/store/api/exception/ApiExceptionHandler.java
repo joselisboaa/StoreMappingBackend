47dca07eb671e6eb63bf546642f53b9758754ac1
joselisboaa <joselisboaa05@gmail.com>
2023-12-12 14:16:16 -0300

    fix: global error handler exception

--- src/main/java/com/map/store/api/exception/ApiExceptionHandler.java
+++ src/main/java/com/map/store/api/exception/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.map.store.api.exception;
 
+import com.map.store.api.exception.StandardError;
 import com.map.store.domain.service.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,10 @@ public class ApiExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<StandardError> handler(Exception exception) {
         final String ERROR_MSG = "Ocorreu um erro no servidor";
+        final String ERROR_DETAILS = "Ocorreu um erro inesperado no servidor, contate os desenvolvedores.";
+        
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        StandardError error = new StandardError(ERROR_MSG, exception.getMessage());
+        StandardError error = new StandardError(ERROR_MSG, exception.getMessage(), ERROR_DETAILS);
 
         return ResponseEntity.status(httpStatus).body(error);
     }
@@ -20,8 +23,9 @@ public class ApiExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException exception) {
         final String ERROR_MSG = "Não encontrado na base de dados";
+        final String ERROR_DETAILS = "A Entidade solicitada não foi encontrada no banco de dados.";
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
-        StandardError error = new StandardError(ERROR_MSG, exception.getMessage());
+        StandardError error = new StandardError(ERROR_MSG, exception.getMessage(), ERROR_DETAILS);
 
         return ResponseEntity.status(httpStatus).body(error);
     }
--- src/main/java/com/map/store/api/exception/StandardError.java
+++ src/main/java/com/map/store/api/exception/StandardError.java
@@ -9,10 +9,12 @@ public class StandardError implements Serializable {
     private static final long serialVersionUID = 1L;
     private String error;
     private String message;
+    private String details;
 
-    public StandardError(String error, String message) {
+    public StandardError(String error, String message, String details) {
         this.error = error;
         this.message = message;
+        this.details = details;
     }
 }
 
