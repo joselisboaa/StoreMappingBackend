92eada657c67af3419161a970a717cfa6c5b921b
joselisboaa <joselisboaa05@gmail.com>
2023-12-12 14:13:59 -0300

    feat: create signature service implementation

---
+++ src/main/java/com/map/store/domain/service/SignatureServiceImpl.java
@@ -0,0 +1,67 @@
+package com.map.store.domain.service;
+
+import com.map.store.domain.model.Signature;
+import com.map.store.domain.repository.ISignatureRepository;
+import com.map.store.domain.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SignatureServiceImpl implements ISignatureService {
+    @Autowired
+    private ISignatureRepository signatureReposity;
+
+    private final String SIGNATURE_NOT_EXIST = "Essa assinatura não existe ou não foi encontrada.";
+
+     public List<Signature> getAll() {
+         return signatureReposity.findAll();
+     }
+
+     public Signature getById(Long id) {
+         Optional<Signature> client = signatureReposity.findById(id);
+
+        return client.orElseThrow(() -> {
+            throw new ResourceNotFoundException(SIGNATURE_NOT_EXIST);
+        });
+     }
+
+     public Signature create(Signature client) {
+         Signature newSignature = signatureReposity.save(client);
+
+         return newSignature;
+     }
+
+     public Signature update(Signature newSignatureData, Long id) {
+         try {
+             Signature clientData = signatureReposity.getReferenceById(id);
+             updateSignatureData(clientData, newSignatureData);
+             
+             if (clientData == null) {
+                 throw new ResourceNotFoundException("Teste");
+             }
+
+             return clientData;
+         } catch (EntityNotFoundException error) {
+             throw new ResourceNotFoundException(SIGNATURE_NOT_EXIST);
+         }
+     }
+
+     public void deleteById(Long id) {
+         try {
+             if (signatureReposity.existsById(id)) {
+                 signatureReposity.deleteById(id);
+             }
+             throw new EntityNotFoundException();
+         } catch (EntityNotFoundException error) {
+             throw new ResourceNotFoundException(SIGNATURE_NOT_EXIST);
+         }
+     }
+
+     private void updateSignatureData(Signature oldSignatureData, Signature newSignatureData) {
+         oldSignatureData.setClient(newSignatureData.getClient());
+     }
+}
